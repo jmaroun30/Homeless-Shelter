7ce1ac7c54055d83c744ff203d8b3c4d747e86fb
Johnny Maroun <JohnnyMaroun@lawn-143-215-50-112.lawn.gatech.edu>
2018-04-18 15:34:40 -0400

    Login UI correct functionality

--- Homeless Shelter/ViewController.swift
+++ Homeless Shelter/ViewController.swift
@@ -1,25 +1,59 @@
 //
 //  ViewController.swift
-//  Homeless Shelter
+//  Uber
 //
 //  Created by Johnny Maroun on 4/18/18.
 //  Copyright © 2018 MarounApps. All rights reserved.
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var adminLabel: UILabel!
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    
+    var signUpMode = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func topTapped(_ sender: Any) {
     }
-
-
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if signUpMode {
+            topButton.setTitle("Login", for: .normal)
+            bottomButton.setTitle("Not Registered? Click here", for: .normal)
+            adminLabel.isHidden = true
+            userLabel.isHidden = true
+            nameTextField.isHidden = true
+            emailTextField.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        } else {
+            topButton.setTitle("Register", for: .normal)
+            bottomButton.setTitle("cancel", for: .normal)
+            adminLabel.isHidden = false
+            userLabel.isHidden = false
+            nameTextField.isHidden = false
+            emailTextField.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+    }
+    
+    
 }
 
